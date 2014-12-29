8039af62c2525c9249f2454cf6db3e2e8bdf401c
ed ive <ed.ivve@gmail.com>
2014-12-29 02:09:56 +0000

    fixed path configuration and config comments

--- logger.sh
+++ logger.sh
@@ -1,31 +1,25 @@
   #!/bin/bash
-  current_dir=$(pwd)
-  entries_dir=$current_dir'/entries'
+
+  # configure the commented lines below. (brackets optional)
+  ando_dir='/home/ive/andolog' # path to andolog file (run $pwd to find this) 
+  entries_dir=$ando_dir'/entries'
   temp_file=$entries_dir'/templog.md'
   log_file=$entries_dir'/log.md'
-  header="# ~log"
 
-  # Open up vim and write output to a temp file
+  header="# ~log" # (the header at the top of the log)
+
   nano $temp_file
-
-  # Once vim is closed, check if the temp file exists
   if [ -f $temp_file ]; then
-      # Assign the temp data to a variable and delete the file
       newlog=$(cat $temp_file)
       rm $temp_file
-
-      # Put all the lines of the existing log into a variable, except
-      # for the header line.
       logdata=$(grep -v "^$header" $log_file)
-
-      # Write the header, new timestamp, new log, and old log data.
       echo -e "$header\n" > $log_file
-      echo -e "**$(date --rfc-3339=seconds)**\n" >> $log_file
+      echo -e "**$(date --rfc-3339=seconds)**\n" >> $log_file # (adds date)
       echo -e "$newlog\n" >> $log_file
       echo -e "$logdata" >> $log_file
-      echo "logged and..."
+      echo "logged and..." # (log confirmation dialogue)
       echo $(bash publisher.sh)
   else
-      echo "not logged, without data"
+      echo "not logged, without data" # (log failure dialogue)
   fi
 
--- publisher.sh
+++ publisher.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-html_dir='/var/www/'
-current_dir=$(pwd)
-entry_dir=$current_dir'/entries'
-template_dir=$current_dir'/entries/templates'
+# configure the commented variables below
+
+html_dir='/var/www/' # directory to which log.html should be output
+ando_dir='/home/ive/andolog' # path to andolog file (run $pwd in file)
+entry_dir=$ando_dir'/entries'
+template_dir=$ando_dir'/entries/templates'
 cd $entry_dir
 
-  # For each of the files with the extension .md in the directory
   for file in *.md; do
-      # store the filename and replace .md with .html
       html=$(echo $file | sed 's/\(.*\.\)md/\1html/')
-      # Insert the contents of the header.html template into the
-      # html file
       cat $template_dir/header.html > $html_dir/$html
-      # The markdown command converts markdown to html. The output
-      # is appended to the html file.
       markdown $file >> $html_dir/$html
-      # Read the contents of the footer and append to the html.
       cat $template_dir/footer.html >> $html_dir/$html
-      printf "published $html // "
+      printf "published $html // " # ( publish message )
       done
-echo "どうもありがとう ～アンド"
+echo "どうもありがとう ～アンド" # (many thanks, ~ando)
