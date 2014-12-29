3afd8fca0c54fc49d208a2172dd203e841e4119f
ed ive <ed.ivve@gmail.com>
2014-12-29 01:08:33 +0000

    fixing up logger.sh to work with filestructure, added log.md file for testing.

--- logger.sh
+++ logger.sh
@@ -1,30 +1,30 @@
   #!/bin/bash
-
-  log='~/.log/log.md'
-  temp='~/./log/templog.md'
-
+  current_dir=$(pwd)
+  entries_dir=$current_dir'/entries'
+  temp_file=$entries_dir'/templog.md'
+  log_file=$entries_dir'/log.md'
   header="# ~log"
 
   # Open up vim and write output to a temp file
-  vim $temp
+  nano $temp_file
 
   # Once vim is closed, check if the temp file exists
-  if [-f $temp ]; then
+  if [ -f $temp_file ]; then
       # Assign the temp data to a variable and delete the file
-      newlog=$(cat $temp)
-      rm $temp
+      newlog=$(cat $temp_file)
+      rm $temp_file
 
       # Put all the lines of the existing log into a variable, except
       # for the header line.
-      logdata=$(grep -v "^$header" $log)
+      logdata=$(grep -v "^$header" $log_file)
 
       # Write the header, new timestamp, new log, and old log data.
-      echo -e "$header\n" > $log
-      echo -e "**$(date --rfc-3339=seconds)**\n" >> $log
-      echo -e "$newlog\n" >> $log
-      echo -e "$logdata" >> $log
+      echo -e "$header\n" > $log_file
+      echo -e "**$(date --rfc-3339=seconds)**\n" >> $log_file
+      echo -e "$newlog\n" >> $log_file
+      echo -e "$logdata" >> $log_file
 
-      echo "don't forget to publish!"
+      echo "don't forget to publish"
   else
       echo "no data"
   fi
