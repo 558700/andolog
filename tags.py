f25816337be066d3ae4055b5a48318e275c5911f
ed ive <ed.ivve@gmail.com>
2014-12-30 01:01:34 +0000

    theoretically working tags.py, need to find a good spot to intercept log with

--- tags.py
+++ tags.py
@@ -1,11 +1,15 @@
-tags=[ '#tag1' ]
+tags=[ '#tag1', '#amet']
 
 entry = 'Lorem ipsum dolor tag #amet #tag1'
 
 
-for item in tags:
-	print item
-	entry = entry.replace(item, '<a id=\'tag1\'>#tag1</a>')
+for tag in tags:
+	print tag
+	word_tag = tag[1:]
+	print "word tag is " + word_tag
+	html_tag = '<a id=\'' + word_tag + '\'>' + tag + '</a>'
+	print "html tag is " + html_tag
+	entry = entry.replace(tag, html_tag)
 
 print "printing replaced entry variable"
 print entry
